7f4dc356ed9feac7fadec728720a060b20e7117b
Anirudh-RG <i-Anirudh@roundglass.com>
2025-04-28 06:18:06 +0000

    init

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const PORT = 3001;
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("Hellow rold")
+    res.status(200);
+})
+
+app.get('/health',(req,res)=>{
+    res.status(200);
+    res.send("OK");
+})
+
+app.listen(PORT,()=>{
+    console.log(`listens on ${PORT}`)
+})
